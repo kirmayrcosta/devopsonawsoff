83edc1103b87bc7b0a53284d0e1faed83e517ce2
kirmayrcosta <62621949+kirmayrcosta@users.noreply.github.com>
2021-03-20 09:55:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! com passo manual: 337016')
 })
 
+app.get('/kirma', (req, res) => {
+  res.send('Teste de pipeline')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
